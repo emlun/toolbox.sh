17f173bf847d7fd2aa39fa1a3a51bbfb71aa6080
Emil Lundberg <lundberg.emil@gmail.com>
2012-08-22 14:50:28 +0200

    Added md5chk.sh
    Added a short script that wraps md5sum and allows specification of the
    correct checksum directly on the command line.

---
+++ md5chk.sh
@@ -0,0 +1,20 @@
+# Wraps md5sum and allows specification of the correct md5sum
+# immediately on the command line.
+
+if [[ $# < 2 ]]; then
+	echo "Usage: md5chk <file> <md5 string>"
+	exit 1
+fi
+
+echo "Correct sum: $2"
+sum=$(md5sum $1 | cut -d \  -f 1)
+echo "File sum:    $sum"
+
+if [[ $sum == $2 ]]; then
+	echo "Match!"
+	exit 0
+else
+	echo "Sums do not match!"
+	exit 2
+fi
+
