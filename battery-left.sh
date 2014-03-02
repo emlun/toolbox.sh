b1d48aae326f5f7be4d9dce08b44c1ddc8d04fa3
Emil Lundberg <lundberg.emil@gmail.com>
2014-03-03 03:48:31 +0900

    Add battery-left.sh

---
+++ battery-left.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if ! hash ibam; then
+    echo "ibam not found!"
+    exit 1
+fi
+
+# Hours and minutes left
+time=$(ibam | grep Battery | grep -oE "([[:digit:]]+:?)+" | cut -d : -f 1-2)
+
+if [[ "$time" == "" ]]; then
+    # Charge time left
+    time=$(ibam | grep Charge | grep -oE "([[:digit:]]+:?)+" | cut -d : -f 1-2)
+    time="CHRG $time"
+fi
+
+# Percentage left
+percent=$(ibam --percentbattery | grep percentage | grep -oE '[[:digit:]]+')
+
+echo "$time ($percent%)"
