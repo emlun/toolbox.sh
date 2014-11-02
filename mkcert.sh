7d8fdc41dacad3f69c4ba73f3d6e799d6357ea13
Emil Lundberg <lundberg.emil@gmail.com>
2014-11-03 00:12:41 +0100

    Extract and rename some variables in mkcert

--- mkcert.sh
+++ mkcert.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-filename="device"
-key_file="${filename}.key"
-request_file="${filename}.csr"
-cert_file="${filename}.crt"
+FILENAME="device"
+KEY_FILE="${FILENAME}.key"
+REQUEST_FILE="${FILENAME}.csr"
+CERT_FILE="${FILENAME}.crt"
 
-upload_file="tmp/${request_file}"
-download_file="tmp/${cert_file}"
+UPLOAD_FILE="tmp/${REQUEST_FILE}"
+DOWNLOAD_FILE="tmp/${CERT_FILE}"
+
+CA_HOST=home
+CA_HOST_CACERT_FILE="/etc/ssl/public/mimer-root-CA.crt"
+CA_HOST_CA_KEY_FILE="/etc/ssl/private/mimer-root-CA.key"
 
 trap 'exit $?' ERR
 
-openssl req -newkey rsa:2048 -keyout "${key_file}" -out "${request_file}" -nodes
+touch "${KEY_FILE}"
+chmod 600 "${KEY_FILE}"
 
-scp "${request_file}" "mimer:${upload_file}"
+openssl req -newkey rsa:2048 -keyout "${KEY_FILE}" -out "${REQUEST_FILE}" -nodes
 
-ssh -t mimer sudo openssl x509 -req -in "'${upload_file}'" -CA /etc/ssl/public/mimer-root-CA.crt -CAkey /etc/ssl/private/mimer-root-CA.key -out "'${download_file}'" -days 365
+scp "${REQUEST_FILE}" "mimer:${UPLOAD_FILE}"
 
-scp "mimer:${download_file}" "${cert_file}"
+ssh -t "${CA_HOST}" sudo openssl x509 -req -in "'${UPLOAD_FILE}'" -CA "'${CA_HOST_CACERT_FILE}'" -CAkey "'${CA_HOST_CA_KEY_FILE}'" -out "'${DOWNLOAD_FILE}'" -days 365
+
+scp "${CA_HOST}":"${DOWNLOAD_FILE}" "${CERT_FILE}"
