cb1f21f25ea724debd50c82ec1c9ca3b60e2d67f
Emil Lundberg <lundberg.emil@gmail.com>
2015-02-10 02:08:55 +0100

    Rename JSON_FILE to PAYLOAD_FILE

--- keybase-prove.sh
+++ keybase-prove.sh
@@ -6,11 +6,11 @@ REMOVABLE_DEVICE_UUID="c5cd54f0-dd6e-4928-be23-dab8a874109b"
 MOUNTPOINT=/mnt/usb
 BASEDIR="${MOUNTPOINT}/keybase"
 KEYBASE_BLOB_FILE="${BASEDIR}/keybase-blob.txt"
-JSON_FILE="${BASEDIR}/keybase.json"
 MAKE_PAYLOAD_SCRIPT="${BASEDIR}/keybase-make-payload.sh"
 SIGN_SCRIPT="${BASEDIR}/keybase-sign.sh"
-SIGNED_FILE="${JSON_FILE}.asc"
 UPLOAD_SCRIPT="${BASEDIR}/keybase-finish.sh"
+PAYLOAD_FILE="${BASEDIR}/keybase.json"
+SIGNED_FILE="${PAYLOAD_FILE}.asc"
 
 sudo mount -U ${REMOVABLE_DEVICE_UUID} "${MOUNTPOINT}"
 sudo mkdir -p "${BASEDIR}"
@@ -25,13 +25,13 @@ cat << EOF > "${MAKE_PAYLOAD_SCRIPT}"
 EOF
 grep -E "^echo" "${KEYBASE_BLOB_FILE}" | cut -d '|' -f 1 >> "${MAKE_PAYLOAD_SCRIPT}"
 chmod a+x "${MAKE_PAYLOAD_SCRIPT}"
-"${MAKE_PAYLOAD_SCRIPT}" > "${JSON_FILE}"
+"${MAKE_PAYLOAD_SCRIPT}" > "${PAYLOAD_FILE}"
 
 # Make script for signing in airgapped environment
 cat << EOF > "${SIGN_SCRIPT}"
 #!/bin/bash
 EOF
-grep -E "^gpg" "${KEYBASE_BLOB_FILE}" | cut -d '|' -f 1 | sed "s/-u '\([0-9a-fA-F]*\)'/-u '\1!'/" | sed "s#sign#sign '${JSON_FILE}'#" >> "${SIGN_SCRIPT}"
+grep -E "^gpg" "${KEYBASE_BLOB_FILE}" | cut -d '|' -f 1 | sed "s/-u '\([0-9a-fA-F]*\)'/-u '\1!'/" | sed "s#sign#sign '${PAYLOAD_FILE}'#" >> "${SIGN_SCRIPT}"
 chmod a+x "${SIGN_SCRIPT}"
 
 # Make script for uploading signature
