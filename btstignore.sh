2c7531613298b63baa1b42eb7c8b32dfb2cfbd24
Emil Lundberg <lundberg.emil@gmail.com>
2015-02-17 15:21:56 +0100

    Add btstignore.sh

---
+++ btstignore.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Add ignore patterns for Syncthing files to BitTorrent Sync ignore file
+cat << EOF >> .sync/IgnoreList
+
+# Syncthing
+.stfolder
+.stignore
+.stversions
+EOF
+
+# Add ignore patterns for Bittorrent Sync files to Syncthing ignore file
+cat << EOF >> .stignore
+# BitTorrent Sync
+.sync
+EOF
